8a9f42c9ad11b7eba77beae9ced26b72489690d2
goosefin <agustinafin96@gmail.com>
2023-06-09 19:02:56 -0400

    Sign in route is working

--- controllers/users.ctrls.js
+++ controllers/users.ctrls.js
@@ -19,41 +19,39 @@ const signUp = async (req,res) =>{
             }
         }
     } catch(err){
-        return res.status(404).json({error:err})
+        return res.status(404).json({error:err.message})
     }
-    // db.User.findOne({username:req.body.username}, (err,userExists) =>{
-    //     if(err){
-    //         res.status(404).json({error:err.message})
-    //     }
-    //     if(userExists){
-    //         res.redirect('/spotussy/login')
-    //     } else {
-    //         db.User.create(req.body, (err,createdUser) =>{
-    //             if(err){
-    //                 return res.status(404).json({error:err.message})
-    //             } else {
-    //                 return res.status(200).json({user: createdUser})
-    //             }
-    //         })
-    //     }
-    // })
 }
 
-const signIn = (req,res) =>{
-    db.User.findOne({email:req.body.email}, (err,foundUser) =>{
-        if(err){
-            return res.status(404).json({error: err.message})
-        }else{
-            if(foundUser){
-                const validLogin = bcrypt.compareSync(req.body.password, foundUser.password)
-                if(validLogin){
-                    return res.status(200).json({foundUser})
-                }else{
-                    return res.status(404).json({error: 'Log in failed'})
-                }
+const signIn = async (req,res) =>{
+    try{
+        const foundUser = await db.User.findOne({email:req.body.email})
+        console.log('Sign In User Found', foundUser)
+        if(foundUser){
+            const validLogin = bcrypt.compareSync(req.body.password, foundUser.password)
+            if(validLogin){
+                return res.status(200).json({foundUser})
+            } else {
+                return res.status(404).json({error:"Log in failed"})
             }
         }
-    })
+    } catch(err){
+        return res.status(404).json({error: err.message})
+    }
+    // db.User.findOne({email:req.body.email}, (err,foundUser) =>{
+    //     if(err){
+    //         return res.status(404).json({error: err.message})
+    //     }else{
+    //         if(foundUser){
+    //             const validLogin = bcrypt.compareSync(req.body.password, foundUser.password)
+    //             if(validLogin){
+    //                 return res.status(200).json({foundUser})
+    //             }else{
+    //                 return res.status(404).json({error: 'Log in failed'})
+    //             }
+    //         }
+    //     }
+    // })
 }
 
 const signOut = (req,res) =>{
